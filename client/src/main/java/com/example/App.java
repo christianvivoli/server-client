4a0c281b1a9e4f03bdf1b31b87f98b964996bc9a
lai2-01 <lai2-01@itismeucci.com>
2022-10-05 09:47:10 +0200

    commit per il client

--- client/src/main/java/com/example/App.java
+++ client/src/main/java/com/example/App.java
@@ -1,13 +1,29 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args )throws Exception
     {
-        System.out.println( "Hello World!" );
+        Socket s= new Socket("localhost",3000);
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("ci sono");
+        pr.flush();
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader br = new BufferedReader(in);
+
+        String str = br.readLine();
+        System.out.println("Server: "+str);
+   
+        s.close();
     }
 }
