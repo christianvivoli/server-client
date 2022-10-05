f506e862efb13fb924d7da0e20af6560051e4bef
lai2-01 <lai2-01@itismeucci.com>
2022-10-05 09:34:24 +0200

    commit per server

--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -1,13 +1,33 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args )throws Exception
     {
-        System.out.println( "Hello World!" );
+        ServerSocket ss = new ServerSocket(3000);
+        System.out.println("server in ascolto sulla porta 3000");
+        Socket s=ss.accept();
+        System.out.println("client connesso");
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader br = new BufferedReader(in);
+        String str = br.readLine();
+        System.out.println("Client: "+str);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("ben arrivato");
+        pr.flush();
+        s.close();
+        ss.close();
     }
 }
